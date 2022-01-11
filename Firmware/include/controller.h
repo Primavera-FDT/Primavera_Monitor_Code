b9c36c5131cdba56f5d10d6cd4b63be6ea0f4595
Folkert Kevelam <folkert@pyllr.nl>
2022-01-11 13:47:49 +0100

    Add global array

--- Firmware/include/controller.h
+++ Firmware/include/controller.h
@@ -4,6 +4,34 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+struct Counting_Semaphore {
+    SemaphoreHandle_t Handle;
+    UBaseType_t Max_Count;
+    UBaseType_t Initial_Count;
+};
+
+struct Queue_Config {
+    QueueHandle_t Queue;
+    unsigned int Elements;
+    unsigned int Size;
+};
+
+SemaphoreHandle_t Binary[] = {
+
+};
+
+struct Counting_Semaphore Counting[] = {
+
+}:
+
+SemaphoreHandle_t Mutex[] = {
+
+};
+
+struct Queue_Config Queue[] = {
+
+};
+
 // The main controller task
 void vController(void *pvParameters);
 
