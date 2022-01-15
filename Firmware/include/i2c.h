ee0d6c2751808dea8c996b95157cd4cf877867c7
Folkert Kevelam <folkert@pyllr.nl>
2022-01-15 12:04:04 +0100

    Changed comm mechanisms

--- Firmware/include/i2c.h
+++ Firmware/include/i2c.h
@@ -8,16 +8,22 @@ typedef enum {
     I2C_BUS_2
 } I2C_Bus;
 
+struct I2C_Address {
+    uint8_t read_address;
+    uint8_t write_address;
+};
+
 typedef unsigned char I2C_Message[20];
 
 struct I2C_MsgPack {
-    uint8_t address;
+    uint8_t id;
+    struct I2C_Address address;
     I2C_Message msg;
     uint8_t msglen;
 };
 
-void I2C_Send(I2C_Bus bus,  uint8_t address, I2C_Message &msg, uint8_t len);
-void I2C_Receive(I2C_Bus bus, uint8_t address, I2C_Message &msg, uint8_t *len);
+uint8_t I2C_Send(I2C_Bus bus, struct I2C_Address addr, I2C_Message &msg, uint8_t len);
+void I2C_Receive(I2C_Bus bus, uint8_t id, struct I2C_Address addr, I2C_Message &msg, uint8_t *len);
 
 void I2C_Peripheral(void *pvparameters);
 
